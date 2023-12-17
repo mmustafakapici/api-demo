705379bf00171a3fe8e9cea210269139890f4dc1
Muhammed Mustafa Kapıcı <ug.mustafa.kapici@toros.edu.tr>
2023-12-17 19:42:16 +0300

    cmt-0

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,4 +1,4 @@
-﻿package handlers
+package handlers
 
 import (
 	"encoding/json"
@@ -9,10 +9,21 @@ import (
 
 // Coin struct'ı CoinGecko API'sinden gelen verileri temsil eder
 type Coin struct {
-	ID     string  `json:"id"`
-	Symbol string  `json:"symbol"`
-	Name   string  `json:"name"`
-	Price  float64 `json:"current_price"`
+	ID        string  `json:"id"`
+	Rank      int     `json:"market_cap_rank"`
+	Symbol    string  `json:"symbol"`
+	Name      string  `json:"name"`
+	Price     float64 `json:"current_price"`
+	Image     string  `json:"image"`
+	Change    float64 `json:"price_change_percentage_24h"`
+	MarketCap float64 `json:"market_cap"`
+}
+
+// HomeHandler, / rotasına hizmet verir
+func HomeHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, World!",
+	})
 }
 
 // PingHandler, /ping rotasına hizmet verir
@@ -24,31 +35,6 @@ func PingHandler(c *gin.Context) {
 
 // ListCoinsHandler, CoinGecko API'sinden verileri alarak /list rotasına hizmet verir
 func ListCoinsHandler(c *gin.Context) {
-	// CoinGecko API'sine GET isteği göndermek için HTTP istemcisi oluşturun
-	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "CoinGecko API'ye bağlanırken bir hata oluştu",
-		})
-		return
-	}
-	defer resp.Body.Close()
-
-	// API yanıtını JSON olarak ayrıştırın
-	var coins []Coin
-	if err := json.NewDecoder(resp.Body).Decode(&coins); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "API yanıtını ayrıştırırken bir hata oluştu",
-		})
-		return
-	}
-
-	// Çekilen verileri JSON olarak yanıtlayın
-	c.JSON(http.StatusOK, coins)
-}
-
-// FetchCoinsHandler, CoinGecko API'sinden verileri alarak /fetchCoins rotasına hizmet verir
-func FetchCoinsHandler(c *gin.Context) {
 	// CoinGecko API'sine GET isteği göndermek için HTTP istemcisi oluşturun
 	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
 	if err != nil {
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
-﻿package main
+package main
 
 import (
 	"api-demo/handlers"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
+
+	// / rotası
+	r.GET("/", handlers.HomeHandler)
 
 	// /ping rotası
 	r.GET("/ping", handlers.PingHandler)
@@ -14,8 +19,13 @@ func main() {
 	// /list rotası
 	r.GET("/list", handlers.ListCoinsHandler)
 
-	// /fetchCoins rotası
-	r.GET("/fetchCoins", handlers.FetchCoinsHandler)
-
 	r.Run(":8080")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(r.Run("0.0.0.0:" + port))
 }
